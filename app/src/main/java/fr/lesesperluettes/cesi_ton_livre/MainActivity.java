533d211e2da350d7bfdb97858876a83b3e701752
Ekode <ekode@protonmail.com>
2021-03-31 09:32:04 +0200

    Added basic bottom navigation for user

--- app/src/main/java/fr/lesesperluettes/cesi_ton_livre/MainActivity.java
+++ app/src/main/java/fr/lesesperluettes/cesi_ton_livre/MainActivity.java
@@ -2,13 +2,25 @@ package fr.lesesperluettes.cesi_ton_livre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(v -> moveToSearchActivity());
+    }
+
+    private void moveToSearchActivity(){
+        Intent searchActivity = new Intent(this,SearchActivity.class);
+        startActivity(searchActivity);
     }
 }
