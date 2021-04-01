94444d7ce430a8868273ac084c645d21a16f0dfa
Théo David <theodavid1997@gmail.com>
2021-04-01 14:06:17 +0200

    Fix on MainActivity to redirect to Login Page / and fixed links

--- app/src/main/java/fr/lesesperluettes/cesi_ton_livre/LoginActivity.java
+++ app/src/main/java/fr/lesesperluettes/cesi_ton_livre/LoginActivity.java
@@ -1,11 +1,11 @@
 package fr.lesesperluettes.cesi_ton_livre;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,9 +15,10 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         btnSignUp = (Button) findViewById(R.id.buttonSignUp);
+        signIn = (TextView) findViewById(R.id.signIn);
         btnSignUp.setOnClickListener(v -> moveToSearchActivity());
         signIn.setOnClickListener(v -> signInActivity());
     }
--- app/src/main/java/fr/lesesperluettes/cesi_ton_livre/SignInActivity.java
+++ app/src/main/java/fr/lesesperluettes/cesi_ton_livre/SignInActivity.java
@@ -1,5 +1,6 @@
 package fr.lesesperluettes.cesi_ton_livre;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,21 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SignInActivity extends AppCompatActivity {
 
     Button btnSignIn;
-    TextView name, firstname, phone, password;
+    TextView name, firstname, phone, password, confirmPassword, address, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
 
         btnSignIn = (Button) findViewById(R.id.register);
         btnSignIn.setOnClickListener(v -> signIn());
     }
 
     private void signIn() {
+        /*name = findViewById(R.id.name);
+        firstname = findViewById(R.id.firstname);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmpassword);
+        address = findViewById(R.id.address);
+        email = findViewById(R.id.email);*/
         // insert into user values
-        findViewById(R.id.name);
-
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
     }
 
 }
